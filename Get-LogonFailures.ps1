2b8971dc5b326ff1498fdf9a9cd9a74aebd428dc
Patrick Gemme <pgemme@holycross.edu>
2020-03-25 14:34:53 -0400

    Update Get-LogonFailures.ps1
    adding top comments

--- Get-LogonFailures.ps1
+++ Get-LogonFailures.ps1
@@ -1,3 +1,18 @@
+<#
+.SYNOPSIS
+This is a script to find the most recent user login failuers on a computer
+.DESCRIPTION
+The script searches the windows security event logs for events created because of login failures.
+It will then output the accounts that attempted to login starting with the more recent.
+The output will include the domain\user and time attempted
+.EXAMPLE
+        Get-Logonfailure terminalserver
+    This will start outputing the most recent login failures to 'terminalserver'
+.NOTES
+Can/will only go back as far as the event log is big
+.LINK
+no link
+#>
 function Get-LogonFailure
 {
       param($ComputerName)
